cefcdb5c7850c5023148802a68677f85fcf0d8cb
BigNate24 <joshjone44@outlook.com>
2023-10-18 08:24:21 -0400

    Initial commit

---
+++ Vehicle.java
@@ -0,0 +1,48 @@
+class Vehicle {
+    private String brand;
+    private int speed;
+    private int distanceTraveled;
+
+    private int currentLapSpeed;
+
+    public Vehicle(String brand, int speed) {
+        this.brand = brand;
+        this.speed = speed;
+        this.distanceTraveled = 0;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getDistanceTraveled() {
+        return distanceTraveled;
+    }
+
+    public void updateDistance(int speed) {
+        // Update the distance traveled based on speed
+        distanceTraveled += speed;
+    }
+
+    public int getCurrentLapSpeed() {
+        return currentLapSpeed;
+    }
+
+    public void setCurrentLapSpeed(int currentLapSpeed) {
+        this.currentLapSpeed = currentLapSpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "brand='" + brand + '\'' +
+                ", speed=" + speed +
+                ", distanceTraveled=" + distanceTraveled +
+                '}';
+    }
+
+}
